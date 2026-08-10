60fa5f380cfd3c482b2d2ec5580c1bf86d5117b5
YnnzID <ynnz.co.id@gmail.com>
2026-08-10 10:38:49 +0700

    Change TypewriterEffect text from 'YnnzID.com' to 'YnnzID'

--- src/Pages/WelcomeScreen.jsx
+++ src/Pages/WelcomeScreen.jsx
@@ -4,6 +4,7 @@ import { Code2, Github, Globe, User } from 'lucide-react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+
 const TypewriterEffect = ({ text }) => {
   const [displayText, setDisplayText] = useState('');
 
@@ -161,7 +162,7 @@ const WelcomeScreen = ({ onLoadingComplete }) => {
                   <div className="relative flex items-center gap-2 text-lg sm:text-xl md:text-2xl">
                     <Globe className="w-4 h-4 sm:w-5 sm:h-5 text-indigo-600" />
                     <span className="bg-gradient-to-r from-indigo-600 to-purple-600 bg-clip-text text-transparent">
-                      <TypewriterEffect text="YnnzID.com" />
+                      <TypewriterEffect text="YnnzID" />
                     </span>
                   </div>
                 </a>
